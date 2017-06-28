e697b2891e41772336102ba6804564af0f05cb11
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-06-28 10:01:12 -0500

    data binding in place for search based on user input-not the best search query ever, but I digress

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -4,13 +4,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      employees: []
+      employees: [],
+      searchFilter: ""
     },
     mounted: function() {
       $.get('http://localhost:3000/api/employeedata', function(responseEmployeeData) {
         console.log(responseEmployeeData);
         this.employees = responseEmployeeData;
       }.bind(this));
+    },
+    computed: {
+      filteredEmployees: function() {
+        var lowerNameFilter = this.searchFilter.toLowerCase();
+        var filtered = this.employees.filter(function(employee) {
+          var lowerName = employee.first_name.toLowerCase() + employee.last_name.toLowerCase() + employee.job_title.toLowerCase();
+          return lowerName.indexOf(lowerNameFilter) !== -1;
+        });
+        return filtered;
+      }
     }
   });
 });
