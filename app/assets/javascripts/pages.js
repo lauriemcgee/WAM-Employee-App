95b36086d7814150cc40d9a83c38abb57de4053e
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-06-27 19:12:14 -0500

    employee data reflected in bulleted list under search box

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,16 @@
+/* global Vue, $ */
+
 document.addEventListener("DOMContentLoaded", function(event) { 
   var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      employees: []
+    },
+    mounted: function() {
+      $.get('http://localhost:3000/api/employeedata', function(responseEmployeeData) {
+        console.log(responseEmployeeData);
+        this.employees = responseEmployeeData;
+      }.bind(this));
     }
   });
 });
